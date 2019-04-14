badc254ad6a26bf0b880b008ef1457337135858d
bebo1995 <a.arrigoni5@studenti.unibg.it>
2019-04-14 15:57:31 +0200

    Update

--- Ascolta.py
+++ Ascolta.py
@@ -1,11 +1,6 @@
 import telepot
 
-file1 = None
-file2 = None
-
 def ascolta_foo(bot,msg,chat_id,persona,i):
-	global file1
-	global file2
 	if i==0:
 		bot.sendMessage(chat_id, 'Posso ascoltare solo 2 persone per'+ 
 		' volta...come si chiama la prima delle due?')
@@ -17,10 +12,14 @@ def ascolta_foo(bot,msg,chat_id,persona,i):
 	if i == 2:
 		bot.sendMessage(chat_id, '%s'%persona[0] + ' e %s'%persona[1] +
 		', vi sto ascoltando!')
-		file1 = open("frasi1.txt","r+")
-		file2 = open("frasi2.txt","r+")
 	if i > 2:
 		if msg["from"]["first_name"] == persona[0]:
-			file1.write(msg['text'])
+			file1 = open("frasi1.txt","a")
+			print('text from %s saved!'%persona[0])
+			file1.write(msg['text'] + '\n')
+			file1.close()
 		if msg["from"]["first_name"] == persona[1]:
+			file2 = open("frasi2.txt","a")
+			print('text from %s saved!'%persona[1])
 			file2.write(msg['text'])
+			file2.close()
